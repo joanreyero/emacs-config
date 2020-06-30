9dbe63930bab90e56cfab3e2101a6d63d65eb535
Joan Reyero <joanreyero@gmail.com>
2020-06-30 19:10:27 +0100

    Updated README

--- .emacs.d/org-clock-save.el
+++ .emacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - MacBook-Pro at <2020-06-30 Tue 18:55>
+;; org-clock-save.el - Joans-iMac at <2020-06-30 Tue 19:09>
 (setq org-clock-stored-history '(("/Users/joanreyero/Dropbox/joanreyero/someday.org" . 1)))
--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Note: it works but the files are a mess. A cleanup is needed.
 ```
 cd ~
 ln -s ~/path/to/clone/.emacs .
+mkdir .emacs.d
 ln -s ~/path/to/clone/.emacs.d .
 ```
 
