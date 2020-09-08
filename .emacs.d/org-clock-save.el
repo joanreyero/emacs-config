59649503b6ab9ff36cfd4162fe0605a9441f953c
Joan Reyero <joanreyero@gmail.com>
2020-09-08 15:40:36 +0100

    Nothing

--- .emacs.d/org-clock-save.el
+++ .emacs.d/org-clock-save.el
@@ -1,2 +1,2 @@
-;; org-clock-save.el - Joans-iMac at <2020-06-30 Tue 19:09>
+;; org-clock-save.el - Joans-iMac.local at <2020-08-26 Wed 18:03>
 (setq org-clock-stored-history '(("/Users/joanreyero/Dropbox/joanreyero/someday.org" . 1)))
